b919124ba747d60f158bc77af2962a9d2030cb22
jpascas <jpascas@gmail.com>
2020-06-13 12:46:44 -0500

    updated data structure function

--- client/Data/dataLoader.js
+++ client/Data/dataLoader.js
@@ -2,7 +2,28 @@ import specializationNodes from './specialization.json';
 import technologiesNodes from './technologies.json';
 
 function GetSpecialistArray() {
-  return specializationNodes;
+  const specializationsModel = specializationNodes;
+  // loop specializations
+  for(let specialization of specializationsModel) {
+    if (specialization.hasOwnProperty("entryNodeId")) {
+        // convert children from linked list to array
+        specialization.technologies = GetTechnologyPathAsOrderedArray(specialization.entryNodeId);
+    }
+  }
+  console.log("specialization ",specializationsModel);
+  return specializationsModel;
+}
+
+function GetTechnologyPathAsOrderedArray(startingNodeId){
+    let orderedArray = [];
+    let currentNodeId  = startingNodeId;
+    while (currentNodeId) {
+        let node = technologiesNodes[currentNodeId];
+        orderedArray.push(node);
+        currentNodeId = node.nextNodeId;
+    }
+    
+    return orderedArray;
 }
 
 export default GetSpecialistArray;
