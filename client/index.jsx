1ea07a3a832d4554b9d1d762b5e393dec1eab0c9
Jeffrey Lu <yumyumyum@yumyumyum-mothership.local>
2020-06-13 19:52:33 -0400

    this commit refactors the camera in the scene

--- client/index.jsx
+++ client/index.jsx
@@ -1,30 +1,16 @@
 import ReactDOM from 'react-dom';
-import React, { useRef, useState, useEffect } from 'react';
-import { Canvas, useFrame, useThree } from 'react-three-fiber';
+import React from 'react';
+import { Canvas } from 'react-three-fiber';
+import { MapControls } from 'drei';
+import './components/styles.css';
 import MasterContainer from './components/MasterContainer.jsx';
-import { MapControls, OrbitControls } from 'drei';
-import GetSpecialistArray from './Data/dataLoader.js';
-import "./components/styles.css";
-
-function Camera(props) {
-  const ref = useRef();
-  const { setDefaultCamera } = useThree();
-  // Make the camera known to the system
-  useEffect(() => setDefaultCamera(ref.current), []);
-  // Update it every frame
-  useFrame(() => ref.current.updateMatrixWorld());
-  return <perspectiveCamera ref={ref} {...props} />;
-}
-
 
 ReactDOM.render(
-  <Canvas colorManagement>
-    <Camera position={[0, 0, 10]} />
+  <Canvas colorManagement camera={{ position: [0, 0, 10] }}>
     <ambientLight />
     <pointLight position={[0, 0, 5]} />
     <MasterContainer />
     <MapControls />
-    <OrbitControls />
   </Canvas>,
   document.getElementById('root'),
 );
