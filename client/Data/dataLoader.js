d22a3d7329dc53c902b905154c241e44b2b64f0b
jpascas <jpascas@gmail.com>
2020-06-13 12:20:36 -0500

    data

---
+++ client/Data/dataLoader.js
@@ -0,0 +1,8 @@
+import specializationNodes from './specialization.json';
+import technologiesNodes from './technologies.json';
+
+function GetSpecialistArray() {
+  return specializationNodes;
+}
+
+export default GetSpecialistArray;
